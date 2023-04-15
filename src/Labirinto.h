2b6646b88cf5adece2f4c3ce6ab9bacab62980e1
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-04-15 15:28:53 -0300

    Create Labirinto.h

---
+++ src/Labirinto.h
@@ -0,0 +1,31 @@
+#ifndef LABIRINTO_H
+#define LABIRINTO_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define QuantMatriz 7
+#include <unistd.h>
+
+typedef struct Matriz{
+char matrix[100][100];
+char caminho[100][100];	
+	} Matriz;
+typedef struct Garoto{	
+int hp;
+int passos;
+int itens;
+int perigos;
+int sacola;
+int Inexplorado;
+
+}Personagem;
+	
+		
+Matriz Matrizes[QuantMatriz];
+int Initiation();
+int GenerateMatrix(int n,int k);
+void PrintMatrix(int n,int k);
+void ReadMatriz();
+int Jornada(int n,int k);
+ 
+#endif
