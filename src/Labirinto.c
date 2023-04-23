0fe0ab2a3a02b05444a61143cbb295ca097384a6
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-04-23 20:20:05 -0300

    Update Labirinto.c

--- src/Labirinto.c
+++ src/Labirinto.c
@@ -1,7 +1,8 @@
 #include "Labirinto.h"
 
-void Initiation()
+void Initiation(Mapa *mat)
 {
+
 	int k,n=0,opcao;
 
 	printf("\n******************************************************\n");	
@@ -16,25 +17,25 @@ void Initiation()
 			printf("Digite o tamanho da matriz quadrada: ");
 			scanf("%d", &n);
 		for(k=0;k<QuantMatriz;k++){
-			GenerateMatrix(n,k);
-			PrintMatrix(n,k);
+			GenerateMatrix(n,k,mat);
+			PrintMatrix(n,k,mat);
 		}
-		Jornada(n,QuantMatriz);
+		Jornada(n,QuantMatriz,mat);
 	
 		}		
 		else if(opcao == 2){			
-		ReadMatriz();		
+		ReadMatriz(mat);		
 		}
 		else if(opcao == 0){
 		
 		}
 		else{
-		printf("\nOpÃ§Ã£o invÃ¡lida\n");
+		printf("\nOpcao invalida\n");
 		}
 
 }
 
-int GenerateMatrix(int n,int k){
+int GenerateMatrix(int n,int k,Mapa *mat){
 	int i,j,data=0,data2=0;
 	FILE *f = fopen("dataset/input.data", "a");
     if (f == NULL) {
@@ -48,30 +49,31 @@ int GenerateMatrix(int n,int k){
    }
 	for(i = 0; i < n; i++){
 		for(j = 0; j < n; j++){
-			Matrizes[k].caminho[i][j]='0';
+			mat->Matrizes[k].caminho[i][j]='0';
 			data = rand()%20;
         	if(data <= 10){     	
-			Matrizes[k].matrix[i][j] = (1 + rand()%5) + '0';
-			 fprintf(f, "%c ", Matrizes[k].matrix[i][j]);
+			mat->Matrizes[k].map[i][j] = (1 + rand()%5) + '0';
+			 fprintf(f, "%c ", mat->Matrizes[k].map[i][j]);
 		}else if ((data > 10) && (data < 16)){
-			Matrizes[k].matrix[i][j] = '*';
-			 fprintf(f, "%c ", Matrizes[k].matrix[i][j]);
+			mat->Matrizes[k].map[i][j] = '*';
+			 fprintf(f, "%c ", mat->Matrizes[k].map[i][j]);
 		} else
 		{
+		
 				if(i==0&&j==0&&k==0)
 			{
 			data2 = rand()%20;
 			if(data2 <= 15){     	
-			Matrizes[k].matrix[i][j] = (1 + rand()%5) + '0';
-			 fprintf(f, "%c ", Matrizes[k].matrix[i][j]);
+			mat->Matrizes[k].map[i][j] = (1 + rand()%5) + '0';
+			 fprintf(f, "%c ", mat->Matrizes[k].map[i][j]);
 		}else if ((data2 > 15) && (data2 < 20)){
-			Matrizes[k].matrix[i][j] = '*';
-			 fprintf(f, "%c ", Matrizes[k].matrix[i][j]);
+			mat->Matrizes[k].map[i][j] = '*';
+			 fprintf(f, "%c ", mat->Matrizes[k].map[i][j]);
 
 		}
 			}else{
-			Matrizes[k].matrix[i][j] = '#';
-			 fprintf(f, "%c ", Matrizes[k].matrix[i][j]);
+			mat->Matrizes[k].map[i][j] = '#';
+			 fprintf(f, "%c ", mat->Matrizes[k].map[i][j]);
 		}
 		}
        
@@ -87,12 +89,12 @@ int GenerateMatrix(int n,int k){
 	return 0;
 }
 
-void PrintMatrix(int n,int k){
+void PrintMatrix(int n,int k,Mapa *mat){
 	int i,j;
 		printf("\nSala %d\n\n",k);
 	for(i = 0; i < n; i++){
 		for(j = 0; j < n; j++){
-			printf("%c ", Matrizes[k].matrix[i][j]);
+			printf("%c ", mat->Matrizes[k].map[i][j]);
 		}
 		printf("\n");
 	}
@@ -100,7 +102,7 @@ void PrintMatrix(int n,int k){
 }
 
 
-void ReadMatriz() {
+void ReadMatriz(Mapa *mat) {
     FILE *f = fopen("dataset/input.data", "r");
     if (f == NULL) {
         printf("Erro ao abrir o arquivo para leitura\n");
@@ -115,7 +117,7 @@ void ReadMatriz() {
     for (k = 0; k < numeroMatrizes; k++) {
         for (i = 0; i < linhas; i++) {
             for (j = 0; j < colunas; j++) {
-            	Matrizes[k].caminho[i][j]='0';
+            	mat->Matrizes[k].caminho[i][j]='0';
                 c = fgetc(f);
                 while (isspace(c)) {
                     c = fgetc(f);
@@ -124,32 +126,31 @@ void ReadMatriz() {
                     if (i == 0 && j == 0 && k == 0) {
                         int data = rand() % 20;
                         if (data <= 10) {     
-                            Matrizes[k].matrix[i][j] = (1 + rand() % 5) + '0';
+                            mat->Matrizes[k].map[i][j] = (1 + rand() % 5) + '0';
                         } else if (data > 10 && data < 16) {
-                            Matrizes[k].matrix[i][j] = '*';
+                            mat->Matrizes[k].map[i][j] = '*';
                         }
                     } else {
-                        Matrizes[k].matrix[i][j] = c;
+                        mat->Matrizes[k].map[i][j] = c;
                     }
                 } else {
-                    Matrizes[k].matrix[i][j] = c;
+                    mat->Matrizes[k].map[i][j] = c;
                 }
             }
         }
     }
 
     for (k = 0; k < QuantMatriz; k++) {
-        PrintMatrix(linhas, k);
+        PrintMatrix(linhas, k,mat);
     }
 
     fclose(f);
-    Jornada(linhas,numeroMatrizes);
+    Jornada(linhas,numeroMatrizes,mat);
 }
 
-int Jornada(int n,int tam)
+int Jornada(int n,int tam,Mapa *mat)
 {
-    
-	Personagem Garoto;
+    Personagem Garoto;
 	int i=0,j=0,r=0,x=0,k=0,data=0,a,c,b,ok=0,opcao,visualizacao=0;
 	int Direita=0,Esquerda=0,Baixo=0,Cima=0,DiagonalEsquerdaSuperior=0,DiagonalDireitaSuperior=0,DiagonalEsquerdaInferior=0,DiagonalDireitaInferior=0;
 	Garoto.hp=10;
@@ -158,7 +159,7 @@ int Jornada(int n,int tam)
 	Garoto.passos=0;
 	Garoto.perigos=0;
 	Garoto.sacola=0;
-	Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+	mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 		printf("\nVisualizar Movimentos do Garoto?\n1-Sim\n2-Nao\n");
 		scanf("%d", &opcao);
 		if(opcao==1)
@@ -176,9 +177,9 @@ int Jornada(int n,int tam)
 	for(;;)
 	{
 	
-	//1° Etapa Verificador de casa
+	//1Â° Etapa Verificador de casa
 	
-		if(Matrizes[k].matrix[i][j]=='*'){	
+		if(mat->Matrizes[k].map[i][j]=='*'){	
     		Garoto.hp=Garoto.hp-1;
     		Garoto.perigos=Garoto.perigos+1;
   
@@ -194,7 +195,7 @@ int Jornada(int n,int tam)
 		 	for(k = 0; k < QuantMatriz; k++){
 		 		for(i = 0; i < n; i++){
 		 		for(j = 0; j < n; j++){		 			
-		 			if(Matrizes[k].matrix[i][j]!='0'&&Matrizes[k].matrix[i][j]!='#'&&Matrizes[k].matrix[i][j]!='*')
+		 			if(mat->Matrizes[k].map[i][j]!='0'&&mat->Matrizes[k].map[i][j]!='#'&&mat->Matrizes[k].map[i][j]!='*')
 		 			{
 		 				Garoto.Inexplorado=Garoto.Inexplorado+1;					 }
 				 
@@ -203,16 +204,16 @@ int Jornada(int n,int tam)
 		 		
 		 	}
 		 printf("\nQuantidade de casas Inexploradas :%d ",Garoto.Inexplorado);
-		 Finalization(n,tam);
+		 Finalization(n,tam,mat);
 		 		
 		return 0;
      	
 	}
 	
-    	}else if(Matrizes[k].matrix[i][j]=='#'){
-	}else if(Matrizes[k].matrix[i][j]>'0'){
-    	Matrizes[k].matrix[i][j]=Matrizes[k].matrix[i][j]-1;	
-    	Matrizes[k].caminho[i][j]=Matrizes[k].caminho[i][j]-1;
+    	}else if(mat->Matrizes[k].map[i][j]=='#'){
+	}else if(mat->Matrizes[k].map[i][j]>'0'){
+    	mat->Matrizes[k].map[i][j]=mat->Matrizes[k].map[i][j]-1;	
+    	mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].caminho[i][j]-1;
     	Garoto.itens=Garoto.itens+1;
     	if(Garoto.sacola<4)
     	{
@@ -224,7 +225,7 @@ int Jornada(int n,int tam)
     	Garoto.sacola=Garoto.sacola+1;
     }
 	   }
-	//2° EtapaVerificador de Sacola
+	//2Â° EtapaVerificador de Sacola
 	if(Garoto.sacola==4)
 	{
 		if(Garoto.hp<10)
@@ -235,48 +236,48 @@ int Jornada(int n,int tam)
 	      	printf("\n |Curou 1 de vida| ");
        }
 	}
-	//3° EtapaDemarcar Paredes
+	//3Â° EtapaDemarcar Paredes
 	//cima
-	if(Matrizes[k].matrix[i-1][j]=='#')
+	if(mat->Matrizes[k].map[i-1][j]=='#')
 	{
 		Cima=-1;
 	}
 	//baixo
-	if(Matrizes[k].matrix[i+1][j]=='#')
+	if(mat->Matrizes[k].map[i+1][j]=='#')
 	{
 		Baixo=-1;
 	}
 	//esquerda
-	if(Matrizes[k].matrix[i][j-1]=='#')
+	if(mat->Matrizes[k].map[i][j-1]=='#')
 	{
 		Esquerda=-1;
 	}
 	//direita
-	if(Matrizes[k].matrix[i][j+1]=='#')
+	if(mat->Matrizes[k].map[i][j+1]=='#')
 	{
 		Direita=-1;
 	}
 	//diagonal superior esquerda
-	if(Matrizes[k].matrix[i-1][j-1]=='#')
+	if(mat->Matrizes[k].map[i-1][j-1]=='#')
 	{
 		DiagonalEsquerdaSuperior=-1;
 	}
 	//diagonal superior direita
-	if(Matrizes[k].matrix[i-1][j+1]=='#')
+	if(mat->Matrizes[k].map[i-1][j+1]=='#')
 	{
 		DiagonalDireitaSuperior=-1;
 	}
 	//diagonal inferior esquerda
-	if(Matrizes[k].matrix[i+1][j-1]=='#')
+	if(mat->Matrizes[k].map[i+1][j-1]=='#')
 	{
 		DiagonalEsquerdaInferior=-1;
 	}
 	//diagonal inferior direita
-	if(Matrizes[k].matrix[i+1][j+1]=='#')
+	if(mat->Matrizes[k].map[i+1][j+1]=='#')
 	{
 		DiagonalDireitaInferior=-1;
 	}
-//4° Etapa Escolher caminho possivel de caminhar
+//4Â° Etapa Escolher caminho possivel de caminhar
 	for(r=0;r<1;)
 	{
 	data = rand()%8;
@@ -299,73 +300,73 @@ int Jornada(int n,int tam)
 	r=1;	
 	}	
    }
-   //5° Etapa Caminhar
+   //5Â° Etapa Caminhar
 	
 	if(data==1)
 	{
 		i=i-1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-			printf("\n |Cima [%c]| ", Matrizes[k].matrix[i][j]);
+			printf("\n |Cima [%c]| ", mat->Matrizes[k].map[i][j]);
 
 
 	}else if(data==2){
 	
 		i=i+1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Baixo [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Baixo [%c]| ", mat->Matrizes[k].map[i][j]);
 
 	}else if(data==3){
 		
 		j=j-1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Esquerda [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Esquerda [%c]| ", mat->Matrizes[k].map[i][j]);
 
 	}else if(data==4){
 		j=j+1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Direita [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Direita [%c]| ", mat->Matrizes[k].map[i][j]);
 
 	}else if(data==5)
 	{
 		i=i-1;
 		j=j+1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Diagonal Direita Superior [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Diagonal Direita Superior [%c]| ", mat->Matrizes[k].map[i][j]);
 
 	}else if(data==6){
 		i=i-1;
 		j=j-1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Diagonal Esquerda Superior [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Diagonal Esquerda Superior [%c]| ", mat->Matrizes[k].map[i][j]);
 	
 	}else if(data==7){
 	    i=i+1;
 	    j=j+1;
 	    Garoto.passos=Garoto.passos+1;
-	    Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+	    mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 	    	if(visualizacao==1)
-	   	printf("\n |Diagonal Direita Inferior [%c]| ", Matrizes[k].matrix[i][j]);
+	   	printf("\n |Diagonal Direita Inferior [%c]| ", mat->Matrizes[k].map[i][j]);
 	
 		
 	}else if(data==8){
 		i=i+1;
 		j=j-1;
 		Garoto.passos=Garoto.passos+1;
-		Matrizes[k].caminho[i][j]=Matrizes[k].matrix[i][j];
+		mat->Matrizes[k].caminho[i][j]=mat->Matrizes[k].map[i][j];
 			if(visualizacao==1)
-		printf("\n |Diagonal Esquerda Inferior [%c]| ", Matrizes[k].matrix[i][j]);
+		printf("\n |Diagonal Esquerda Inferior [%c]| ", mat->Matrizes[k].map[i][j]);
 	}else
 	{
 		
@@ -381,7 +382,7 @@ int Jornada(int n,int tam)
 		r=0;
 	
 
-	//6° Etapa Verificador de Portal
+	//6Â° Etapa Verificador de Portal
 	if(i==0){
 	
 	data = rand()%tam;	
@@ -393,7 +394,7 @@ int Jornada(int n,int tam)
 				printf("\n\n |Passou para a sala %d| \n", data);
 			}
 
-	if(Matrizes[k].matrix[i][j]=='#')
+	if(mat->Matrizes[k].map[i][j]=='#')
 	{
 	k=x;	
 		if(visualizacao==1)
@@ -412,7 +413,7 @@ int Jornada(int n,int tam)
 		}
 
 	
-	if(Matrizes[k].matrix[i][j]=='#')
+	if(mat->Matrizes[k].map[i][j]=='#')
 	{
 	k=x;
 		if(visualizacao==1)
@@ -429,7 +430,7 @@ int Jornada(int n,int tam)
 		}
 
 	
-	if(Matrizes[k].matrix[i][j]=='#')
+	if(mat->Matrizes[k].map[i][j]=='#')
 	{
 	k=x;
 		if(visualizacao==1)
@@ -447,7 +448,7 @@ int Jornada(int n,int tam)
 		}
 
 	
-	if(Matrizes[k].matrix[i][j]=='#')
+	if(mat->Matrizes[k].map[i][j]=='#')
 	{
 	k=x;
 		if(visualizacao==1)
@@ -456,16 +457,16 @@ int Jornada(int n,int tam)
 	
 	}
 
-	//7° Etapa Verificar se está no inicio
+	//7Â° Etapa Verificar se estÃ¡ no inicio
 	if(i==0&&j==0)
 	{
 		ok=1;
 		for(a = 0; a < tam; a++){
 		for(b = 0; b < n; b++){
 		for(c = 0; c < n; c++){
-		if(Matrizes[a].caminho[b][c]!='0')
+		if(mat->Matrizes[a].caminho[b][c]!='0')
 		{
-				if(Matrizes[a].caminho[b][c]!='*')
+				if(mat->Matrizes[a].caminho[b][c]!='*')
 				{					
 					ok=0;	
 				}
@@ -485,7 +486,7 @@ int Jornada(int n,int tam)
 		 	for(k = 0; k < tam; k++){
 		 		for(i = 0; i < n; i++){
 		 		for(j = 0; j < n; j++){		 			
-		 			if(Matrizes[k].matrix[i][j]!='0'&&Matrizes[k].matrix[i][j]!='#'&&Matrizes[k].matrix[i][j]!='*')
+		 			if(mat->Matrizes[k].map[i][j]!='0'&&mat->Matrizes[k].map[i][j]!='#'&&mat->Matrizes[k].map[i][j]!='*')
 		 			{
 		 				Garoto.Inexplorado=Garoto.Inexplorado+1;					 
 						 }
@@ -495,7 +496,7 @@ int Jornada(int n,int tam)
 		 		
 		 	}
 		 printf("\nQuantidade de casas Inexploradas :%d ",Garoto.Inexplorado);
-			Finalization(n,tam);
+			Finalization(n,tam,mat);
 		 		 
 		 return 0;
 		}
@@ -507,7 +508,7 @@ int Jornada(int n,int tam)
 	
 }
 
-void Finalization(int n,int tam)
+void Finalization(int n,int tam,Mapa *mat)
 {
 	int k,i,j;
 	FILE *g = fopen("dataset/output.data", "a");
@@ -520,8 +521,8 @@ void Finalization(int n,int tam)
 	for(k = 0;k < tam; k++){
 	for(i = 0; i < n; i++){
 		for(j = 0; j < n; j++){
-			printf("%c ", Matrizes[k].matrix[i][j]);
-			 fprintf(g, "%c ", Matrizes[k].matrix[i][j]);
+			printf("%c ", mat->Matrizes[k].map[i][j]);
+			 fprintf(g, "%c ", mat->Matrizes[k].map[i][j]);
 		}
 		printf("\n");
 		 fprintf(g, "\n");
@@ -535,7 +536,6 @@ void Finalization(int n,int tam)
 
 
 	
-
 
 
 
